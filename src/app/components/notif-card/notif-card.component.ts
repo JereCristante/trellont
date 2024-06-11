7e34b2eeaa8ac235dfd1ae8a6335ce267c110d4a
Jeremias Cristante <jeremias_cristante@itrsa.com.ar>
2024-06-11 16:44:13 -0300

    first commit

---
+++ src/app/components/notif-card/notif-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notif-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './notif-card.component.html',
+  styleUrl: './notif-card.component.css'
+})
+export class NotifCardComponent {
+
+}
